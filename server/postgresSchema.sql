c3757d8cd55106a4ba4e3a86e0501817a4f78555
KKD <kianakdonaldson@gmail.com>
2020-01-03 10:17:11 -0800

    Create postgres seed with foreign key constraints

--- server/postgresSchema.sql
+++ server/postgresSchema.sql
@@ -3,38 +3,53 @@ CREATE SCHEMA ReservationsModule;
 
 SET client_min_messages = error;
 
+DROP TABLE IF EXISTS Restaurants CASCADE;
 CREATE TABLE IF NOT EXISTS Restaurants (
-  id int not null unique primary key,
+  restaurant_id int not null,
   name text,
   open time,
   close time,
   num_seats smallint,
-  reservations int
+  reservations int,
+  CONSTRAINT restaurant_id PRIMARY KEY (restaurant_id)
 );
 
+DROP TABLE IF EXISTS Users CASCADE;
+CREATE TABLE IF NOT EXISTS Users (
+  user_id int not null,
+  username text,
+  first_name text,
+  last_name text,
+  email text,
+  phone_number text,
+  note text,
+  CONSTRAINT user_id PRIMARY KEY (user_id)
+);
+
+DROP TABLE IF EXISTS Reservations CASCADE;
+-- ALTER TABLE IF EXISTS Reservations
+--   DROP CONSTRAINT reservations_user_id_fkey;
+-- ALTER TABLE IF EXISTS Reservations
+--   DROP CONSTRAINT reservations_restaurant_id_fkey;
 CREATE TABLE IF NOT EXISTS Reservations (
-  id int not null unique primary key,
+  reservation_id int not null,
   restaurant_id int,
   user_id int,
-  date date,
+  date text,
   time time,
   party_size smallint,
-  FOREIGN KEY (restaurant_id) REFERENCES restaurants (id),
-  FOREIGN KEY (user_id) REFERENCES users (id)
+  FOREIGN KEY (restaurant_id) REFERENCES restaurants (restaurant_id),
+  FOREIGN KEY (user_id) REFERENCES users (user_id),
+  CONSTRAINT reservation_id PRIMARY KEY (reservation_id)
 );
 
-CREATE TABLE IF NOT EXISTS Users (
-  id int not null unique primary key,
-  username text,
-  firstName text,
-  lastName text,
-  email text,
-  phoneNumber smallint,
-  note text
-);
+-- ANALYZE VERBOSE;
 
-ANALYZE VERBOSE;
+COPY restaurants(restaurant_id,name,open,close,num_seats,reservations) 
+FROM '/Users/keeyanakellydonaldson/Documents/team-200ok/reservations-service/restaurants.csv' DELIMITER ',';
 
-COPY restaurants(id,name,open,close,num_seats,reservations) 
-FROM '/Users/keeyanakellydonaldson/Documents/team-200ok/reservations-service/restaurants.csv' DELIMITER ',' CSV HEADER;
+COPY users(user_id,username,first_name,last_name,email,phone_number,note)
+FROM '/Users/keeyanakellydonaldson/Documents/team-200ok/reservations-service/users.csv' DELIMITER ',';
 
+COPY reservations(reservation_id,restaurant_id,user_id,date,time,party_size)
+FROM '/Users/keeyanakellydonaldson/Documents/team-200ok/reservations-service/reservations.csv' DELIMITER ',';
